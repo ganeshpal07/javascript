80480bbbe20e815fb61e40ebdadd964546ff2199
Ganesh <ganeshpal1516@gmail.com>
2026-02-02 10:47:26 +0530

    hello

--- script.js
+++ script.js
@@ -19,11 +19,71 @@
 
 
 
-let count = 1
-const intervalId = setInterval(()=>{
-    console.log(count)
-    count++;
-    if(count>10){
-        clearInterval(intervalId)
-    }
-}, 1000)
+// let count = 1
+// const intervalId = setInterval(()=>{
+//     console.log(count)
+//     count++;
+//     if(count>10){
+//         clearInterval(intervalId)
+//     }
+// }, 1000)
+
+
+// function print(){     // callback function
+
+//     console.log("hello students!!")
+// }
+
+// function greet(num){    // higher order function
+//     console.log("welcome to my classs!!")
+//     num()
+// }
+// greet(print)
+
+// function greet(num, message){    // higher order function
+//     console.log("welcome to my classs!!")
+//     setTimeout(()=>{
+//         console.log("inside timeout")
+//         let firstName = "john"
+//         num(firstName)
+//     }, 2000);
+
+// }
+
+// greet(print)
+
+    
+console.log("starting homework...");
+
+setTimeout(() => {
+    console.log("homework completed!");
+    console.log("starting dinner...");
+    setTimeout(() => {
+        console.log("dinner completed!");
+        console.log("getting ready to go out...");
+        setTimeout(() => {
+            console.log("going to playground!");
+        }, 1000); 
+        
+    }, 1500);
+
+}, 2000);     
+
+
+function finishHomework(callback){
+    console.log("starting homework...");
+
+    setTimeout(() => {
+        console.log("homework completed!");
+        callback();
+    }, 2000);
+}
+
+function eatdinner(callback){
+    console.log("starting dinner...");
+    setTimeout(() => {
+        console.log("dinner completed!");
+        callback();
+    }, 1500);
+
+}
